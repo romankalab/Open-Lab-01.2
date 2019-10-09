db47e4b9d259476bf78741212540aab7db8ca164
romankalab <roan.kalab@mail.com>
2019-10-09 19:26:00 +0200

    Initial

---
+++ Open-Lab-01.2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace Open_Lab_01._1
+{
+    class GFG
+    {
+        public static void Main()
+        {
+            int number;
+
+            Console.WriteLine("Enter your number: ");
+
+            number = Convert.ToInt32(Console.ReadLine());
+
+            int number2;
+
+            Console.WriteLine("Enter your ssecond number: ");
+
+            number2 = Convert.ToInt32(Console.ReadLine());
+
+            int sum = number + number2;
+
+            Console.WriteLine($"This is the result: {sum}");
+        }
+    }
+}
